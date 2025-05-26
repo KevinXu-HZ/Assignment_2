2ad31e7fcfa5c1e770d877d836ee1dc5caf0555f
KevinXu-HZ <northwindxu@gmail.com>
2025-05-26 01:21:05 -0700

    Bug fixing

--- src/main/kotlin/com/example/demo/controller/WebController.kt
+++ src/main/kotlin/com/example/demo/controller/WebController.kt
@@ -1,13 +1,14 @@
 package com.example.demo.controller
 
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
 class WebController {
 
     @GetMapping("/")
-    fun index(): String {
+    fun index(model: Model): String {
         return "index"
     }
 }
